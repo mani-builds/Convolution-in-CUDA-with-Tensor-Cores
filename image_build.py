202b099d4bc4b81785cf246b80171f695e67ee18
mani-builds <manikantatjea973@gmail.com>
2026-03-12 12:50:46 -0700

    Working model of naive

---
+++ image_build.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import numpy as np
+
+def image_build():
+    # f_name = input("Enter the file for output of gemm in text: ")
+    # a_name = input("Enter the file for shape of the input array in text: ")
+    # o_name = input("Enter the file for output in jpg: ")
+    f_name = 'output_gemm_identity.txt'
+    a_name = 'dog_array_shape.txt'
+    o_name = 'image_output_identity.jpg'
+    with open(f_name, 'r') as f:
+        output = np.loadtxt(f)
+    with open(a_name, 'r') as a:
+        shape = np.loadtxt(a)
+    n_channels, height, width = shape.astype(int)
+    print(n_channels, height, width)
+    output = output.reshape([height, width, n_channels])
+    print("Reshaped: ", output.shape)
+    output = output.astype(np.uint8)
+    print(output)
+    print(type(output))
+    im_o = Image.fromarray(output)
+    im_o.save(o_name)
+
+image_build()
